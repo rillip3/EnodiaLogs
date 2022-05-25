2cf480551005e3586d47957370df0533427d5a75
Philip Eatherington <philip.eatherington@rackspace.com>
2022-05-25 18:49:27 -0400

    Initial commit, works for me

--- README.md
+++ README.md
@@ -1,2 +1,69 @@
 # Enodia
 A python wrapper around kubectl logs to make searches easier
+
+In short, this is a version of git for Kubernetes logs. It will allow you to 
+go through the logs for a specified container name for every pod that matches
+a simple string search. It will then go through the log file and provide all
+the lines that match your search terms, exclude any lines that have excluded
+terms, and provide the requested number of lines before or after each instance
+of the search term.
+
+# Requirements
+Hecate only requires the official Kubernetes Python library. You can run
+
+pip install -r requirements.txt
+
+To install it on most systems.
+
+# Examples
+
+python3 enodia.py -o output.txt -s mytext1 mytext2 -c container_name \
+    -p partial_pod_name -n namespace_name -x exclude1 exclude2 \
+    -a 10 -b 5
+
+This will look at the logs for container container_name in every pod that has
+partial_pod_name in it for the namespace namespace_name. Any lines that have
+exclude1 or exclude2 will be removed from the search results. Five lines before
+the result and ten lines after the result will be included in the output.
+
+This is analagous to 
+
+kubectl logs -c container_name -n namespace_name pod_name | grep -v exclude1 \
+    | grep -v exclude2 | grep 'mytext1\|mytext2' -A10 -B10
+
+The only difference is that it will search all pods with a match on the pod
+name, rather than the one specific pod.
+
+
+# Usage
+
+A utility to search kubernetes log files
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -a AFTER [AFTER ...], --after AFTER [AFTER ...]
+                        lines after the search result to include                                                                                                                                                  
+  -b BEFORE [BEFORE ...], --before BEFORE [BEFORE ...]                                                                                                                                                            
+                        lines after the search result to include                                                                                                                                                  
+  -c CONTAINER [CONTAINER ...], --container CONTAINER [CONTAINER ...]                                                                                                                                             
+                        container to search in                                                                                                                                                                    
+  -p POD [POD ...], --pod POD [POD ...]                                                                                                                                                                           
+                        pod name to limit search results                                                                                                                                                          
+  -x EXCLUDE [EXCLUDE ...], --exclude EXCLUDE [EXCLUDE ...]
+                        a comma separated list of strings to exclude
+  -s SEARCH [SEARCH ...], --search SEARCH [SEARCH ...]
+                        a comma separated list of strings to search for.
+  -o OUT [OUT ...], --out OUT [OUT ...]
+                        a filename to store results
+  -n NAMESPACE [NAMESPACE ...], --namespace NAMESPACE [NAMESPACE ...]
+                        Namespace to search in
+
+LICENSE
+
+Copyright 2022 Philip Eatherington
+
+Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
---
+++ enodia.py
@@ -0,0 +1,159 @@
+'''Enodia: a small utility to search Kubernetes log output.'''
+
+from collections import defaultdict
+import yaml
+import argparse
+from kubernetes import client, config
+from pprint import pprint
+from copy import deepcopy
+
+class FixedSizedList:
+    """A special type of list that never gets to more than N elements, if N is specified"""
+    def __init__(self, size):
+        self.data = []
+        try:
+            int(size)
+        except TypeError:
+            self.size = 0
+        else:
+            self.size = int(size)
+
+    def append(self, entry):
+        if self.size <= 0:
+            return
+            # can't store any data
+        if len(self.data) == self.size:
+            self.data.pop(0)
+            # remove first entry before appending new data
+        self.data.append(entry)
+    
+    def get_data(self):
+        return self.data
+
+    def empty(self):
+        self.data = []
+
+
+class Enodia:
+    def __init__(self) -> None:
+        config.load_kube_config()
+        self.client = client.CoreV1Api()
+
+    def get_pod_names(self, namespace):
+        pod_list = self.client.list_namespaced_pod(namespace=namespace).to_dict()['items']
+        toReturn = []
+        for entry in pod_list:
+            toReturn.append(entry['metadata']['name'])
+        return toReturn
+
+    def get_logs(self, podname, namespace, container=None):
+        logs = defaultdict()
+        podNames = self.get_pod_names(namespace)
+        for entry in podNames:
+            if podname in entry:
+                if not container:
+                    logs[podname] = self.client.read_namespaced_pod_log(entry, namespace=namespace, async_req=True)
+                else:
+                    logs[podname] = self.client.read_namespaced_pod_log(entry, namespace=namespace, container=container, async_req=True)
+        for entry in logs.keys():
+            # currently there's a thread; get it's results and put it in the dict instead
+            logs[entry] = logs[entry].get()
+        return logs
+
+    def search_logs(self, logs, searchTerms, exclude=[], before=0, after=0):
+        try:
+            after = int(after)
+        except TypeError:
+            after = 0
+        beforeBuffer = FixedSizedList(before)
+        searchArea = deepcopy(logs)
+        results = defaultdict(list)
+        for key in searchArea.keys():
+            searchArea[key] = searchArea[key].split('\n') # turn the big text blob to lines
+            for term in searchTerms:
+                for line in searchArea[key]:
+                    if term not in line and not before:
+                        continue
+                        # required term missing, skip
+                    if term not in line and before:
+                        beforeBuffer.append(line)
+                        # save this entry for future use
+                        continue
+                        # required term missing, skip
+                    for exclusion in exclude:
+                        if exclusion in line:
+                            continue  # an excluded term is present, skip
+                    if before:  # the term is here, and they want the before buffer
+                        results[key].extend(beforeBuffer.get_data())
+                        beforeBuffer.empty() # flush items we already added
+                    results[key].append(line)
+        if after:
+            for key in results.keys():
+                cur_index = list(searchArea.keys()).index(key)
+                for index in range(1, after):
+                    try:
+                        # go to the entry in search area for the current key,
+                        # then grab the list entry N entries away from the start
+                        results[key].append(searchArea[key][cur_index + index])
+                    except IndexError:
+                        # we hit the end, no reason to look for more
+                        break
+        return results
+
+
+
+def process(arguments):
+    enodia = Enodia()
+    # parse_args creates a namespace that contains a list of lists. So 
+    # arguments.exclude = [[text1, text2]]
+    # grab the actual values out of the 0th element for lists, [0][0] for strings
+    after = 0
+    before = 0
+    container = None
+    namespace = 'default'
+    pod = arguments.pod[0][0]  # pod is required
+    search = arguments.search[0]  # search is required, might be a list
+    exclude = []
+    if arguments.exclude:
+        exclude = arguments.exclude[0]  # might be a list
+    if arguments.after:
+        after = arguments.after[0][0]
+    if arguments.before:
+        before = arguments.before[0][0]
+    if arguments.namespace:
+        namespace = arguments.namespace[0][0]
+    logs = enodia.get_logs(pod, namespace, container=container)
+    results = enodia.search_logs(logs, search, exclude=exclude, before=before, after=after)
+    if arguments.out[0][0]:
+        with open(arguments.out[0][0], 'a') as outFile:
+            pprint(results, stream=outFile)
+        return 'Output written to %s' % arguments.out[0][0]
+    else:
+        pprint(results)
+
+if __name__ == "__main__":
+    '''
+    Just do some parsinng here, then hand off the parsed arguments to the
+    process method to do the actual work.'''
+    parser = argparse.ArgumentParser(
+        description='A utility to search kubernetes log files')
+    parser.add_argument('-a', '--after', action='append', nargs='+',
+                        help='lines after the search result to include')
+    parser.add_argument('-b', '--before', action='append', nargs='+',
+                        help='lines after the search result to include')
+    parser.add_argument('-c', '--container', action='append', nargs='+',
+                        help='container to search in')
+    parser.add_argument('-p', '--pod', action='append', nargs=1,
+                        required=True,
+                        help='pod name to limit search results')
+    parser.add_argument('-x', '--exclude', action='append', nargs='+',
+                        help='a comma separated list of strings to exclude')
+    parser.add_argument('-s', '--search', action='append', nargs='+',
+                        required=True,
+                        help='a comma separated list of strings to search for.')
+    parser.add_argument('-o', '--out', action='append', nargs='+',
+                        help='a filename to store results')
+    parser.add_argument('-n', '--namespace', action='append', nargs='+',
+                        help='Namespace to search in')
+    arguments = parser.parse_args()
+    pprint(process(arguments))
